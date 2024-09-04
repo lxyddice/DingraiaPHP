3afcce47b8b5d39886825be981f2e4d9498bfe26
lxyddice <lxyddice@qq.com>
2024-09-04 18:06:55 +0800

    修复webhook无效响应时报错

--- module/DingraiaPHP/app/webhook/main.php
+++ module/DingraiaPHP/app/webhook/main.php
@@ -1,6 +1,5 @@
 <?php
-/* webhook */
-/* 是。收webhook的也有webhook... */
+
 if ($bot_run_as) {
     if (file_exists("config/module/webhook/config.json")) {
         $webhookConfig = read_file_to_array("config/module/webhook/config.json");
@@ -14,10 +13,14 @@ if ($bot_run_as) {
             foreach ($webhookConfig["sendWebhook"] as $webhook) {
                 if (isset($webhook["sendChatMode"])) {
                     if (in_array($bot_run_as["chat_mode"], $webhook["sendChatMode"])) {
-                        $res = requests("POST", $webhook["url"], json_decode(file_get_contents("php://input"), true), $webhookHeader)["body"];
+                        $res = requests("POST", $webhook["url"], json_decode(file_get_contents("php://input"), true), $webhookHeader, $webhook["timeout"]);
                         if (count($webhook["callback"]) > 0) {
                             foreach ($webhook["callback"] as $callback) {
-                                send_message($res, $callback);
+                                if (isset($res["body"])) {
+                                    send_message($res["body"], $callback);
+                                } else {
+                                    send_message("Webhook发送时无法获取回调响应body", $callback);
+                                }
                             }
                         }
                     }
